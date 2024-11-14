13e4170189df7938e1bf5fda9b64de6552b5def2
Rafee <rafeehasan88@gmail.com>
2024-11-14 22:40:48 +0600

    rendered conditional data on dynamic card

--- js/video.js
+++ js/video.js
@@ -27,11 +27,12 @@ const displayVideos = (videos) => {
     card.classList = 'card card-compact';
     card.innerHTML = `
 
-         <figure class="h-[200px]">
+         <figure class="h-[200px] relative">
     <img
       src=${video.thumbnail}
       class="h-full w-full object-cover"
       alt="Shoes" />
+      <span class="absolute right-2 bottom-2 text-white bg-black rounded p-1">${video.others.posted_date}</span>
   </figure>
   <div class="px-0 py-2 flex gap-2">
       <div>
@@ -43,7 +44,9 @@ const displayVideos = (videos) => {
         <h2 class="font-bold">${video.title}</h2>
       <div class="flex items-center gap-2">
       <p class="text-gray-400">${video.authors[0].profile_name}</p>
-      <img class="w-5" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png" alt="">
+
+      ${video.authors[0].verified == true ? `<img class="w-5" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png" alt="">` : ""}
+      
       </div
       <p></p>
   
